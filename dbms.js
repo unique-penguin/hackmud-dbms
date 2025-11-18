d89dc2951bb8037ca20ed3c8b5da645aa3406963
Levadas <existenciasocial.astronaut236@passinbox.com>
2025-11-18 04:14:16 +0000

    Error handling and some otimizations

--- dbms.js
+++ dbms.js
@@ -1,37 +1,29 @@
 function(C, A) // cmd:"cmd"
 {	
-	const L = #fs.scripts.lib()
+	var confirm = {ok:false, msg:"Confirm with confirm:true"}
 
-		function debug(info, text){
-			var time = ("000" + (Date.now()-_ST)).slice(-4)
-			#D("(debug) `K|` "+time+"`Ams` `K|` `3[`"+info+"`3]` `K|` "+text)
-		}
-
-
-
-	var HOW_TO_USE = ""+
-		"Database Management Script [v2.0.0]"+
-		"\n"+
+	var HELP = "`HDatabase Management Script [v2.0.0]`"+
+		"\nDependencies: scripts.lib\n"+
 		"\nScript Arguments:"+
 		"\n#`Ncmd`:"+
-		"\n╠`V\"find\"` `v[f]` - Finds an object in the database"+
-		"\n║ #`Nquery`: `V<{`obj`V}>` (default:{} only in find)"+
-		"\n║ #`Nmethod`: (default:\"array\")"+
+		"\n╠`V\"find\"` `v[f]` - Finds a/multiple document(s) in the database"+
+		"\n║ #*`Nquery`: `V<{`obj`V}>` (default:{})"+
+		"\n║ #*`Nmethod`: (default:\"array\")"+
 		"\n║ ╠`V\"first\"` `v[f]` - Returns the first matching document"+
-		"\n║ ║ #`Nprojection`: optional `V<{`obj`V}>`"+
-		"\n║ ║ #`Nskip`: optional `V<`int`V>` (default:0)"+
-		"\n║ ║ #`Nsort`: optional `V<{`obj`V}>`"+
+		"\n║ ║ #*`Nprojection`: `V<{`obj`V}>`"+
+		"\n║ ║ #*`Nskip`: `V<`int`V>`"+
+		"\n║ ║ #*`Nsort`: `V<{`obj`V}>`"+
 		"\n║ ║"+
-		"\n║ ╠`V\"array\"` `v[a]` - Returns all matching documents"+
-		"\n║ ║ #`Nprojection`: optional `V<{`obj`V}>`"+
-		"\n║ ║ #`Nskip`: optional `V<`int`V>` (default:0)"+
-		"\n║ ║ #`Nlimit`: optional `V<`int`V>` (default:10)"+
-		"\n║ ║ #`Nsort`: optional `V<{`obj`V}>`"+
+		"\n║ ╠`V\"array\"` `v[a]` - Returns an array of all matching documents"+
+		"\n║ ║ #*`Nprojection`: `V<{`obj`V}>`"+
+		"\n║ ║ #*`Nskip`: `V<`int`V>`"+
+		"\n║ ║ #*`Nlimit`: `V<`int`V>` (default:10)"+
+		"\n║ ║ #*`Nsort`: `V<{`obj`V}>`"+
 		"\n║ ║"+
 		"\n║ ╠`V\"count\"` `v[c]` - Returns the number of matching documents"+
 		"\n║ ║"+
 		"\n║ ╚`V\"distinct\"` `v[d]` - Returns an array of all unique values found for matching keys"+
-		"\n║   #`Nkey`:`V<\"`property`V\">`"+
+		"\n║   #`Nkey`:`V<\"`property as str`V\">`"+
 		"\n║"+
 		"\n╠`V\"insert\"` `v[i]` - Adds to the database"+
 		"\n║ #`Ndoc`: `V<{`obj`V}>` or `V<[`arr of objs`V]>`"+
@@ -42,97 +34,114 @@ function(C, A) // cmd:"cmd"
 		"\n║ #`Nconfirm`: `Vtrue`"+
 		"\n║"+
 		"\n╠`V\"update\"` `v[u]` - Updates documents in the database"+
-		"\n║ #`Nquery`: `V<{`obj`V}>`"+
-		"\n║ #`Nupdate`: `V<{`obj or obj containing MongoDB update operator`V}>`"+
-		"\n║ #`Nconfirm`: `Vtrue`"+
-		"\n║"+
 		"\n╠`V\"update1\"` `v[u1]` - Updates a single document in the database."+
+		"\n╠`V\"updateS\"` `v[uS]` - Updates documents in the database, if they exist. Inserts the document if it doesn't exist"+
 		"\n║ #`Nquery`: `V<{`obj`V}>`"+
-		"\n║ #`Nupdate`: `V<{`obj or obj containing MongoDB update operator`V}>`"+
+		"\n║ #`Nupdate`: `V<{`obj/MongoDB obj`V}>`"+
 		"\n║ #`Nconfirm`: `Vtrue`"+
 		"\n║"+
-		"\n╠`V\"updateS\"` `v[us]` - Updates documents in the database, if they exist. Inserts the document if it does not exist"+
-		"\n║ #`Nquery`: `V<{`obj`V}>`"+
-		"\n║ #`Nupdate`: `V<{`obj or obj containing MongoDB update operator`V}>`"+
-		"\n║ #`Nconfirm`: `Vtrue`"+
+		"\n╠`V\"specifications\"` `v[specs]`"+
 		"\n║"+
-		"\n╠`V\"specifications\"` `v[specs]` - Specifications of the database"+
-		"\n║"+
-		"\n║`V\"nuke\"` `w(DO NOT USE)` - Deletes everything in the database \\(T^T)/"+
-		"\n╚ #`Nconfirm`:`Vtrue`"+
-		"\n Dependencies: `Escripts.lib`"
+		"\n║`V\"nuke\"` `w(CAUTION)` - Deletes everything in the database \\(T^T)/"+
+		"\n╚ #`Nconfirm`:`Vtrue`\n"+
+		"\n`A(* - optional)`"
 
 
 
-	function findDistinct(){
-		if(!A.key){
-			return{ok:false, msg:"Invalid `Nkey`."}
-		}
-
-		return #db.f(A.query).distinct(A.key)
+	function isObj(x){
+		return (typeof x === 'object' && !Array.isArray(x) && x !== null)
 	}
 
-	function findArray(query, skip, limit, sort){
-		return #db.f(query, A.projection).skip(skip).limit(limit).sort(sort).array()
+	function isInt(x){
+		return Number.isInteger(x)
 	}
-		
+
+	function objSize(x){
+		return Object.keys(x).length
+	}
+
+	
+
 	function find(){
-		var skip = 0
-		var limit = 10
-		var sort = {}
-		var query = {}
-
-		if(L.is_obj(A.query)){
-			query = A.query
-		}
-
-		if(L.is_num(A.skip)){
-			skip = A.skip
-		}
-
-		if(L.is_num(A.limit)){
-			limit = A.limit
+		if(!A.query){
+			A.query = {}
 		}
 		
-		if(L.is_obj(A.sort) && A.sort != {}){
-			sort = A.sort
+		if(!isObj(A.query)){
+			throw new TypeError ("Invalid query")
+		}
+
+		if(!A.skip){
+			A.skip = 0
+		}
+
+		if(!isInt(A.skip)){
+			throw new TypeError("Invalid skip")
+		}
+
+		if(!A.limit){
+			A.limit = 10
+		}
+
+		if(!isInt(A.limit)){
+			throw new TypeError("Invalid limit")
+		}
+
+		if(!A.sort){
+			A.sort = {}
+		}
+		
+		if(!isObj(A.sort)){
+			throw new TypeError("Invalid sort")
 		}
 
 		if(!A.method){
-			return findArray(query, skip, limit, sort)
+			A.method = "a"
 		}
 
 		switch(A.method){
 			case "first":
 			case "f":
-				return #db.f(query, A.projection).skip(skip).sort(sort).first()
+				return #db.f(A.query, A.projection).skip(A.skip).sort(A.sort).first()
 
 			case "array":
 			case "a":
-				return findArray(query, skip, limit, sort)
+				return #db.f(A.query, A.projection).skip(A.skip).limit(A.limit).sort(A.sort).array()
 
 			case "count":
 			case "c":
-				return #db.f(query).count()
+				return #db.f(A.query).count()
 
 			case "distinct":
 			case "d":
-				return findDistinct()
-			
+				if(!A.key){
+					throw new Error("Missing key")
+				}
+
+				if(typeof A.key !== 'string'){
+					throw new TypeError("Invalid key")
+				}
+
+				return #db.f(A.query).distinct(A.key)
+
 			default:
-				return {ok:false, msg:"Invalid `Nmethod`."}
+				throw new Error("Invalid method");
 		}
 	}
 
 
 
 	function insert(){
-		if(!L.is_obj(A.doc)){
-			return {ok:false, msg:"Invalid `Ndoc`."}
+		if(!A.doc){
+			throw new Error("Missing doc")
+		}
+
+		if(!isObj(A.doc)){
+			throw new TypeError("Invalid doc")
 		}
 
 		if(!A.confirm === true){
-			return {ok:false, msg:"Operation not `Nconfirm``ned`."}
+			return confirm
 		}
 
 		return #db.i(A.doc)
@@ -141,12 +150,20 @@ function(C, A) // cmd:"cmd"
 
 
 	function remove(){
-		if(!L.is_obj(A.query) || A.query == {}){
-			return {ok:false, msg:"Invalid `Nquery`."}
+		if(!A.query){
+			throw new Error ("Missing query")
+		}
+
+		if(!isObj(A.query)){
+			throw new TypeError ("Invalid query")
+		}
+
+		if(objSize(A.query) === 0){
+			return {ok:false, msg:"To delete everything use cmd:\"nuke\""}
 		}
 
 		if(!A.confirm === true){
-			return {ok:false, msg:"Operation not `Nconfirm``ned`."}
+			return confirm
 		}
 
 		return #db.r(A.query)
@@ -155,77 +172,77 @@ function(C, A) // cmd:"cmd"
 
 
 	function update(type){
-		if(!L.is_obj(A.query) || A.query == {}){
-			return {ok:false, msg:"Invalid `Nquery`."}
+		if(!A.query){
+			throw new Error ("Missing query")
 		}
 
-		if(!L.is_obj(A.update)){
-			return {ok:false, msg:"Invalid `Nupdate`."}
+		if(!isObj(A.query)){
+			throw new TypeError ("Invalid query")
+		}
+
+		if(!A.update){
+			throw new Error("Missing update")
+		}
+
+		if(!isObj(A.update)){
+			throw new TypeError("Invalid update")
 		}
 
 		if(!A.confirm === true){
-			return {ok:false, msg:"Operation not `Nconfirm``ned`."}
+			return confirm
 		}
 
 		switch(type){
-			case "normal":
+			case "n":
 				return #db.u(A.query, A.update)
 
-			case "one":
-				return #db.u(A.query, A.update)
+			case "o":
+				return #db.u1(A.query, A.update)
 
-			case "upsert":
-				return #db.u(A.query, A.update)
-			
-			default:
-				return {ok: false, msg:"Invalid update type."}
+			case "u":
+				return #db.us(A.query, A.update)
 		}
 	}
 
 
 
-	function specifications(){
-		var specifications = {
+	function specs(){
+		var specs = {
 			n_documents: #db.f({}).count(),
 			collections: #db.f({}).distinct("collection")
 		}
 
-		specifications.collections.forEach((element, index, array) => {
-			array[index] = {
-				name: element,
-				n_documents: #db.f({collection: element}).count()
+		specs.collections.forEach((e, i, a) => {
+			a[i] = {
+				name: e,
+				n_documents: #db.f({collection: e}).count()
 			}
 		})
 
-		return specifications
+		return specs
 	}
 
 
 
 	function nuke(){
 		if(!A.confirm === true){
-			return {ok:false, msg:"Operation not `Nconfirm``ned`."}
+			return confirm
 		}
 
 		if(A.security_message !== "Delete everything in my database"){
-			var warning = "- You are about to `Ddelete ALL documents`!\n"+
-			"- I you know what you are doing type:\nsecurity_message: \"Delete everything in my database\""
-
-			return {ok:false, msg:warning}
+			return {ok:false, msg:"- You are about to `Ddelete ALL documents`!\n- If you know what you are doing type:\nsecurity_message: \"Delete everything in my database\""}
 		}
 
 		return #db.r({})
 	}
-
-
-
-	//function help(pag){
-	//	return {ok:false, msg:"Not implemented yet."}
-	//}
-
+	
 	
 	
 	function commands(){
+		if(!A.cmd){
+			throw new Error("Missing cmd")
+		}
+
 		switch(A.cmd){
 			case "find":
 			case "f":
@@ -241,40 +258,39 @@ function(C, A) // cmd:"cmd"
 
 			case "update":
 			case "u":
-				return update("normal")
+				return update("n")
 
 			case "update1":
 			case "u1":
-				return update("one")
+				return update("o")
 
 			case "updateS":
 			case "uS":
-				return update("upsert")
+				return update("u")
 			
 			case "specifications":
 			case "specs":
-				return specifications()
+				return specs()
 
 			case "nuke":
 				return nuke()
 
-			default: 
-				return {ok:false, msg:"Invalid `Ncmd`"}
+			default:
+				throw new Error ("Invalid cmd")
 		}
 	}
 	
 
 
-	// Order of execution and debug text ----------------------------------------------------------------------------
+	// Order of execution ----------------------------------------------------------------------------
 	try{
-		if((!A) || A == {}){
-			return HOW_TO_USE
+		if((!A)){
+			return HELP
 		}
 
 		return commands()
 
 	}catch(e){
-		#D(e)
-		throw e
+		return "`D"+e+"`"
 	}
 }
