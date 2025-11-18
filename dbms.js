48adba7b9ea471c782b4c5320cba6c40c79a4256
Levadas <existenciasocial.astronaut236@passinbox.com>
2025-11-18 16:44:02 +0000

    Legend of stuff

--- dbms.js
+++ dbms.js
@@ -52,7 +52,7 @@ function(C, A) // cmd:""
 		"\n║"+
 		"\n║`V\"nuke\"` `w(CAUTION)` - Deletes everything in the database \\(T^T)/"+
 		"\n╚ #`Nconfirm`:`Vtrue`\n"+
-		"\n`A(* - optional)`"
+		"\n`A(* - optional) ([f] - aliase)`"
 
 
 
